98dcf794f451061c9df4962a82d0b129c84632d2
Sergeydigl3 <Sergeydigl3@gmail.com>
2021-09-17 04:57:53 +0300

    lab1 finished; added readme.md

---
+++ lab1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{  
+    int x, maxx = 0;
+    while (1) {
+        scanf_s("%d", &x);
+        if (x == 0) 
+            break;
+        else {
+            if (x > maxx || maxx==0) { maxx = x; }
+        };
+    };
+    printf("%d", maxx);
+    return 0;
+}
