f3f8307b70c3ba258cef948701de4c02486d5cf6
Sergeydigl3-Notebook <26508358+Sergeydigl3@users.noreply.github.com>
2021-09-18 03:39:27 +0300

    detail in TODO

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,35 +1,54 @@
 #include <stdio.h>
 
-
-// int main(int argc, char const *argv[])
-// {
-//     char str[100];
-//     fgets(str, 10, stdin);
-//     fflush(stdin);
-    
-//     // printf("%d", );
-//     return 0;
-// }
-
 int main(int argc, char const *argv[])
 {
-    for (int x=0;x<=300;x++){
-        printf("%c %d %d\n", x,x, is_char(x));
+    int size = 300;
+    char str[300] = {0};
+    int size_obr = 100;
+    char obr[100] = {0};
+
+    int otvets[10] = {0};
+
+    printf("Строка: ");
+    fgets(str, size + 1, stdin);
+    fflush(stdin);
+    // TODO: Make Len Size for  (for cycle)
+    printf("Образец поиска: ");
+    fgets(obr, size_obr + 1, stdin);
+    fflush(stdin);
+
+    int obr_pos = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (
+            (is_char(str[i])==1) && (is_char(obr[i])==1) && (str[i] == obr[obr_pos]))
+        {
+            obr_pos++;
+        }
+        
+        if (str[i] != obr[obr_pos]){
+            obr_pos = 0;
+            printf("Позиция %d", i);
+        }
+    }
+}
+
+int is_not_free(char *mass, int dlina)
+{
+    for (int x = 0; x < dlina; x++)
+    {
+        if (mass[x] != '\0')
+            return 1;
     }
     return 0;
 }
 
-
-
-int is_char(int ch){
-    printf("%d", ch);
-    if ( 
-        (65<=ch&&ch<=90) || (97<=ch&&ch<=122)
-        ||
-        (128<=ch&&ch<=175) || (224<=ch&&ch<=239)
-    )
+int is_char(int ch)
+{
+    if (
+        (65 <= ch && ch <= 90) || (97 <= ch && ch <= 122) ||
+        (128 <= ch && ch <= 175) || (224 <= ch && ch <= 239))
         return 1;
     else
         return 0;
-
 }
