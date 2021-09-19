99b7bd3e8c9371ece0524c423d7549d89a34ef20
Sergeydigl3-PC <26508358+Sergeydigl3@users.noreply.github.com>
2021-09-20 01:46:37 +0300

    close to complete

--- mylabs/lab03/lab03.c
+++ mylabs/lab03/lab03.c
@@ -2,6 +2,33 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Lab03");
+    int tmass[] = {1,20, 34, 52};
+
+
     return 0;
+}
+
+void find_close(int *mass, int x, int size){
+    int mindif=-1;
+    int pos1=-1;
+    int pos2=-1;
+
+    for (int i=0;i<size;i++){
+        int diff = abs(x-mass[i]);
+        if (mindif==-1 || diff<=mindif){
+            if (diff==mindif)
+                pos2 = i;
+            else
+                pos1 = i;
+            printf("%d",diff);
+            mindif = diff;
+        }
+    }
+
+}
+
+int abs(int x){
+    if (x<0)
+        return x*(-1);
+    return x;
 }
