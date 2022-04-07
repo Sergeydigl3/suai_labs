02a7978e6aeb36da18dc64a7f1ecc2e8d50c1c5b
Sergeydigl3-Codeserver <26508358+Sergeydigl3@users.noreply.github.com>
2022-04-07 21:32:07 +0300

    turlikov

--- practice/hometask.cpp
+++ practice/hometask.cpp
@@ -9,10 +9,24 @@ int counter(int data)
         w = w + (x & 1);
         x = x >> 1;
     }
-    // printf("%d", w);
 
     return w;
 }
+
+char arr[16] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
+int counter2(int data)
+{
+    int x = 1, w = 0;
+    x = data;
+    while (x > 0)
+    {
+        w = w + arr[x & 15];
+        x = x >> 4;
+    }
+
+    return w;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -20,13 +34,13 @@ int main(int argc, char *argv[])
     int mass[9]={0};
     for (int i = 0; i < count; i++)
     {
-        mass[counter(i)]++;
+        mass[counter2(i)]++;
     }
     printf("\n");
 
     for (size_t i = 0; i < 9; i++)
     {
-        printf("%d - %d\n", i,mass[i]);
+        printf("%ld - %d\n", i, mass[i]);
     }
     return 0;
 }
