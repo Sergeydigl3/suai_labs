369dced4364d402082bc08a7a8018ee6b0aa2bf5
Sergeydigl3-Codeserver <26508358+Sergeydigl3@users.noreply.github.com>
2022-05-16 00:50:13 +0300

    file2 works fine with cycle

--- practice2/file2.cpp
+++ practice2/file2.cpp
@@ -22,68 +22,28 @@ int main()
     int mark[size] = { 0 };
     int CodeBook[size] = { 0 };
 
-    // шаг 1. Выбираем первое кодовое слово.
-    CodeBook[0] = 0;
-    // Отметим взятое слово в массиве пометок.
-    mark[0] = 2;
-    M++;
-    // Шаг 2. \отметим все слова в кандатах на расстоянии меньше d от выбранного.
-    for (int i = 0; i < size;i++)
-    {
-        if (mark[i] == 0 && (count_ones(i ^ CodeBook[M - 1]) < D))
-        {
-            mark[i] = 1;
-
-        }
-        printf("%d ", mark[i]);
-    }
-    printf("\n");
-
-    //Шаг 1.
-  // шаг 1. Выбираем второе кодовое слово.
     int zero = 0;
-    for (zero = 0; mark[zero] != 0; zero++);
-    //printf("%d",zero);  
-    CodeBook[M] = zero;
-    // Отметим взятое слово в массиве пометок.
-    mark[zero] = 2;
-    M++;
-    // Шаг 2. \отметим все слова в кандатах на расстоянии меньше d от выбранного.
-    for (int i = 0; i < size;i++)
-    {
-        if (mark[i] == 0 && (count_ones(i ^ CodeBook[M - 1]) < D))
-        {
-            mark[i] = 1;
-
-        }
-        printf("%d ", mark[i]);
-    }
-    //Шаг 1.
-    printf("\n");
-    // шаг 1. Выбираем второе кодовое слово.
-    zero = 0;
-    for (zero = 0; mark[zero] != 0; zero++);
-    //printf("%d",zero);  
-    CodeBook[M] = zero;
-    // Отметим взятое слово в массиве пометок.
-    mark[zero] = 2;
-    M++;
-    // Шаг 2. \отметим все слова в кандатах на расстоянии меньше d от выбранного.
-    for (int i = 0; i < size;i++)
-    {
-        if (mark[i] == 0 && (count_ones(i ^ CodeBook[M - 1]) < D))
-        {
-            mark[i] = 1;
-
-        }
-        printf("%d ", mark[i]);
-    }
-
-
     
+    while (zero<size)
+    { 
+        CodeBook[M] = zero;
+        mark[zero] = 2;
+        M++;
+
+        for (int i = 0; i < size;i++)
+        {
+            if (mark[i] == 0 && (count_ones(i ^ CodeBook[M - 1]) < D))
+            {
+                mark[i] = 1;
+
+            }
+            printf("%d ", mark[i]);
+        }
+        for (zero = 0; (mark[zero] != 0 && zero<size); zero++);
+        printf("\n");
+    }
+
     printf("\n%d,%d,%d", CodeBook[0], CodeBook[1], CodeBook[2]);
-
-
 
 
     return 0;
