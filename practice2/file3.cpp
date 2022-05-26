c6aca794e071c6e0f7b0088cc22599a430af04cf
Sergeydigl3-Codeserver <26508358+Sergeydigl3@users.noreply.github.com>
2022-05-26 22:31:49 +0300

    3d task finished

--- practice2/file3.cpp
+++ practice2/file3.cpp
@@ -1,12 +1,14 @@
+typedef unsigned long long uint64;
+
 #include <iostream>
 #include <math.h>
-#define N 8
+#define N 5
 #define D 3
 
 using namespace std;
 
-unsigned long long count_ones(unsigned long long n) {
-    unsigned long long w = 0;
+uint64 count_ones(uint64 n) {
+    uint64 w = 0;
     while (n > 0) {
         w += n & 1;
         n = n >> 1;
@@ -14,14 +16,21 @@ unsigned long long count_ones(unsigned long long n) {
     return w;
 }
 
+string str_bin(uint64 n, uint64 size) {
+    string ret_str;
+    for (unsigned char i = 0; i < size; i++)
+        ret_str += to_string((n >> (size - 1 - i)) & 1);
+    return ret_str;
+}
+
 int main()
 {
-    int M = 0;
-    unsigned long long size = pow(2, N);
-    int mark[size] = { 0 };
-    unsigned long long CodeBook[size] = { 0 };
+    unsigned int M = 0;
+    uint64 size = pow(2, N);
+    uint64 mark[size] = { 0 };
+    uint64 CodeBook[size] = { 0 };
 
-    int zero = 0;
+    uint64 zero = 0;
     
     while (zero<size)
     { 
@@ -29,22 +38,22 @@ int main()
         mark[zero] = 2;
         M++;
 
-        for (unsigned long long i = 0; i < size;i++)
+        for (uint64 i = 0; i < size;i++)
         {
             if (mark[i] == 0 && (count_ones(i ^ CodeBook[M - 1]) < D))
             {
                 mark[i] = 1;
 
             }
-            // cout << mark[i] << " ";
+            cout << mark[i] << " ";
         }
         for (zero = 0; (mark[zero] != 0 && zero<size); zero++);
-        // cout << endl;
+        cout << endl;
     }
 
-    cout << "Answers: ";
-    for (size_t i = 0; i < M; i++) cout << CodeBook[i] << " ";
-    cout << endl << endl << M;
+    cout << "Answers: " << endl;
+    for (size_t i = 0; i < M; i++) cout << str_bin(CodeBook[i], N) << " " << CodeBook[i] << endl;
+    cout << endl << endl << M << endl;
 
     return 0;
 }
