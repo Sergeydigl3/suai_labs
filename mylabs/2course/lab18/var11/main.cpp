d7c057e7ab3744e52de32eb16cb4a4f536d00447
Sergeydigl3-Codeserver <26508358+Sergeydigl3@users.noreply.github.com>
2022-09-03 17:42:12 +0300

    Golden code. Load feature complete

--- mylabs/2course/lab18/var11/main.cpp
+++ mylabs/2course/lab18/var11/main.cpp
@@ -16,22 +16,46 @@
 #include "Settings.h"
 using namespace std;
 
+// int main(int argc, char const *argv[])
+// {
+//     Settings sets;
+
+
+//     sets.add("color1", "blue");
+//     sets.add("color1", "blue");
+//     sets.add("color2", "red");
+//     sets.add("color3", "white");
+//     sets.add("colo2r", 2);
+
+//     cout << sets << "\n----------------------------------\nAfter Delete operation\n";
+
+//     sets.erase_str("color2");
+//     cout << sets << "\n\n----------------------------------\n";
+//     cout << "Try find.\ncolor3 = " << sets.find_str("color3") << "\n";
+
+//     sets.save("aboba.txt");
+//     return 0;
+// }
+
+
 int main(int argc, char const *argv[])
 {
     Settings sets;
 
+    sets.load("aboba.txt");
 
-    sets.add("color1", "blue");
-    sets.add("color1", "blue");
-    sets.add("color2", "red");
-    sets.add("color3", "white");
-    sets.add("colo2r", 2);
-
-    cout << sets << "\n----------------------------------\nAfter Delete operation\n";
-
-    sets.erase_str("color2");
     cout << sets << "\n\n----------------------------------\n";
-    cout << "Try find.\ncolor3 = " << sets.find_str("color3") << "\n";
+    // sets.add("color1", "blue");
+    // sets.add("color1", "blue");
+    // sets.add("color2", "red");
+    // sets.add("color3", "white");
+    // sets.add("colo2r", 2);
+
+    // cout << sets << "\n----------------------------------\nAfter Delete operation\n";
+
+    // sets.erase_str("color2");
+    // cout << sets << "\n\n----------------------------------\n";
+    // cout << "Try find.\ncolor3 = " << sets.find_str("color3") << "\n";
 
     sets.save("aboba.txt");
     return 0;
