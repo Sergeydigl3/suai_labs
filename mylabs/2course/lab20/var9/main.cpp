af3a602a77903387dd02798fdc10dcc7bfefd4d3
Sergeydigl3-Notebook <26508358+Sergeydigl3@users.noreply.github.com>
2022-09-05 15:36:52 +0300

    save

--- mylabs/2course/lab20/var9/main.cpp
+++ mylabs/2course/lab20/var9/main.cpp
@@ -1,7 +1,29 @@
+#include <iostream>
+#include <vector>
+
+typedef std::vector<std::vector<int>> TArray;
+
+class Graphs
+{
+private:
+    /* data */
+public:
+    Graphs(/* args */);
+    ~Graphs();
+};
+
+Graphs::Graphs(/* args */)
+{
+}
+
+Graphs::~Graphs()
+{
+}
 
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    TArray a;
+
     return 0;
 }
